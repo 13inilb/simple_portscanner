d84675bb5fd34f913534e120cdb6ea19d4e8b290
Binil Babu P <13inilb@gmail.com>
2024-11-29 19:01:01 +0530

    Add files via upload

---
+++ simple_portscanner.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+target_ip=$1
+start=$2
+stop=$3
+
+if [ $# -ne 3 ]; then
+	echo "Usage : simple_portscanner.sh <target ip> <start port> <end port>"
+	exit 1
+fi
+
+echo "Scanning.."
+for ((port=$start; port<=$stop; port++ ))
+do
+	(echo > /dev/tcp/$target_ip/$port) &>/dev/null && echo "Port $port is open" || echo "Port $port is closed"
+done
+	
